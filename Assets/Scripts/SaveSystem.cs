9e9675611f8ab48d7864b7b359b282fc8b3524d9
LucasFrazaoDev <lucas_devfrazao@outlook.com>
2023-10-26 20:53:22 -0300

    Complete save system base

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class SaveSystem : MonoBehaviour
+{
+    public static SaveSystem Instance { get; private set; }
+
+    private string m_filePath = "notas.json";
+    private List<Invoice> m_invoices = new List<Invoice>();
+    private Dictionary<string, Invoice> m_invoicesByName = new Dictionary<string, Invoice>();
+    private Dictionary<string, Invoice> m_invoicesByLicensePlate = new Dictionary<string, Invoice>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        m_filePath = Path.Combine(Application.persistentDataPath, "invoices.json");
+    }
+
+    private void Start()
+    {
+        LoadNotes();
+    }
+
+    public void SaveNotes()
+    {
+        string json = JsonUtility.ToJson(m_invoices);
+        File.WriteAllText(m_filePath, json);
+    }
+
+    public void LoadNotes()
+    {
+        if (!File.Exists(m_filePath))
+        {
+            Debug.Log("Não há nenhum arquivo salvo!");
+            return;
+        }
+
+        string json = File.ReadAllText(m_filePath);
+        m_invoices = JsonUtility.FromJson<List<Invoice>>(json);
+
+        m_invoicesByName.Clear();
+        m_invoicesByLicensePlate.Clear();
+
+        foreach (var invoice in m_invoices)
+        {
+            m_invoicesByName[invoice.Name] = invoice;
+            m_invoicesByLicensePlate[invoice.LicensePlate] = invoice;
+        }
+    }
+
+
+    public void AddNewNote(Invoice newInvoice)
+    {
+        m_invoices.Add(newInvoice);
+        m_invoicesByName[newInvoice.Name] = newInvoice;
+        m_invoicesByLicensePlate[newInvoice.LicensePlate] = newInvoice;
+        SaveNotes();
+    }
+
+    public Invoice GetNotaPorNome(string name)
+    {
+        if (m_invoicesByName.TryGetValue(name, out Invoice invoice))
+        {
+            return invoice;
+        }
+        return null;
+    }
+
+    public Invoice GetNotaPorPlaca(string licensePlate)
+    {
+        if (m_invoicesByLicensePlate.TryGetValue(licensePlate, out Invoice invoice))
+        {
+            return invoice;
+        }
+        return null;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,6 +11,8 @@ public class UIManager : MonoBehaviour
     private VisualElement m_root;
 
     private Button m_quitButton;
+    private Button m_saveNoteButton;
+    private Button m_searchNoteButton;
 
     private TextField m_nameTextField;
     private TextField m_licensePlateTextField;
@@ -20,25 +22,80 @@ public class UIManager : MonoBehaviour
 
     private PopupField<string> m_popUpField;
 
-    private List<string> m_dropdownOptions = new List<string> { "Placa", "Nome" };
+    private TextField m_searchFilterTextField;
+    private TextField m_consultedNameTextField;
+    private TextField m_consultedLicensePlateTextField;
+    private TextField m_consultedDateTextField;
+    private TextField m_consultedServiceTextField;
+
+    private List<string> m_popupFieldOptions = new List<string> { "Placa", "Nome" };
 
     private void Awake()
     {
         m_root = GetComponent<UIDocument>().rootVisualElement;
 
         m_quitButton = m_root.Q<Button>("QuitButton");
+        m_saveNoteButton = m_root.Q<Button>("AddNewNote");
+        m_searchNoteButton = m_root.Q<Button>("SearchNoteButton");
+        m_dateTimeLabel = m_root.Q<Label>("TimeLabel");
+
         m_nameTextField = m_root.Q<TextField>("NameTextField");
         m_licensePlateTextField = m_root.Q<TextField>("LicensePlateTextField");
         m_serviceTextField = m_root.Q<TextField>("ServiceTextField");
 
-        m_dateTimeLabel = m_root.Q<Label>("TimeLabel");
-
+        m_searchFilterTextField = m_root.Q<TextField>("SearchFilterTextField");
         m_popUpField = m_root.Q<PopupField<string>>("ConsultFilterDropdownField");
-        m_popUpField.choices = m_dropdownOptions;
+        m_consultedNameTextField = m_root.Q<TextField>("ConsultedNameTextField");
+        m_consultedLicensePlateTextField = m_root.Q<TextField>("ConsultedLicensePlateTextField");
+        m_consultedDateTextField = m_root.Q<TextField>("ConsultedDateTextField");
+        m_consultedServiceTextField = m_root.Q<TextField>("ConsultedServiceTextField");
+        
+    }
 
+    private void OnEnable()
+    {
+        m_popUpField.choices = m_popupFieldOptions;
         m_popUpField.RegisterValueChangedCallback(OnPopupFieldValueChanged);
 
         m_quitButton.clicked += OnQuitButtonClicked;
+        m_saveNoteButton.clicked += OnSaveButtonClicked;
+        m_searchNoteButton.clicked += OnSearchNoteButtonClicked;
+    }
+
+    private void Start()
+    {
+        ClearTextFields();
+    }
+
+    private void OnSearchNoteButtonClicked()
+    {
+        Debug.Log("Achei a sua nota!!");
+    }
+
+    private void OnSaveButtonClicked()
+    {
+        string name = m_nameTextField.value;
+        string licensePlate = m_licensePlateTextField.value;
+        string service = m_serviceTextField.value;
+        string date = System.DateTime.Now.ToString();
+
+        Invoice newInvoice = new Invoice(name, licensePlate, service, date);
+        SaveSystem.Instance.AddNewNote(newInvoice);
+        ClearTextFields();
+        Debug.Log("Nota Salva!!");
+    }
+
+    private void ClearTextFields()
+    {
+        m_nameTextField.value = "";
+        m_licensePlateTextField.value = "";
+        m_serviceTextField.value = "";
+
+        m_searchFilterTextField.value = "";
+        m_consultedNameTextField.value = "";
+        m_consultedLicensePlateTextField.value = "";
+        m_consultedDateTextField.value = "";
+        m_consultedServiceTextField.value = "";
     }
 
     private void OnPopupFieldValueChanged(ChangeEvent<string> e)
