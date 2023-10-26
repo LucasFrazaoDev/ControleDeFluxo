89c9c3fae8c14fb2db661b12402d922040b87916
LucasFrazaoDev <lucas_devfrazao@outlook.com>
2023-10-25 23:01:51 -0300

    Working on the layout

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,17 +10,20 @@ public class UIManager : MonoBehaviour
 {
     private VisualElement m_root;
 
-    private Button m_newNoteButton;
-    private Button m_consultNoteButton;
     private Button m_quitButton;
+
+    private TextField m_nameTextField;
+    private TextField m_licensePlateTextField;
+    private TextField m_serviceTextField;
 
     private void Awake()
     {
         m_root = GetComponent<UIDocument>().rootVisualElement;
 
-        m_newNoteButton = m_root.Q<Button>("NewNoteButton");
-        m_consultNoteButton = m_root.Q<Button>("ConsultNoteButton");
         m_quitButton = m_root.Q<Button>("QuitButton");
+        m_nameTextField = m_root.Q<TextField>("NameTextField");
+        m_licensePlateTextField = m_root.Q<TextField>("LicensePlateTextField");
+        m_serviceTextField = m_root.Q<TextField>("ServiceTextField");
 
         m_quitButton.clicked += OnQuitButtonClicked;
     }
