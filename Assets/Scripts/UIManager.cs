cd377d187f925e3e379c978f43a09109865e9617
LucasFrazaoDev <lucas_devfrazao@outlook.com>
2023-10-26 09:30:10 -0300

    PopupField added

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -16,6 +16,10 @@ public class UIManager : MonoBehaviour
     private TextField m_licensePlateTextField;
     private TextField m_serviceTextField;
 
+    private PopupField<string> m_popUpField;
+
+    private List<string> m_dropdownOptions = new List<string> { "Placa", "Nome" };
+
     private void Awake()
     {
         m_root = GetComponent<UIDocument>().rootVisualElement;
@@ -25,7 +29,17 @@ public class UIManager : MonoBehaviour
         m_licensePlateTextField = m_root.Q<TextField>("LicensePlateTextField");
         m_serviceTextField = m_root.Q<TextField>("ServiceTextField");
 
+        m_popUpField = m_root.Q<PopupField<string>>("ConsultFilterDropdownField");
+        m_popUpField.choices = m_dropdownOptions;
+
+        m_popUpField.RegisterValueChangedCallback(OnPopupFieldValueChanged);
+
         m_quitButton.clicked += OnQuitButtonClicked;
+    }
+
+    private void OnPopupFieldValueChanged(ChangeEvent<string> e)
+    {
+        Debug.Log("Selected value: " + e.newValue);
     }
 
     private void OnQuitButtonClicked()
